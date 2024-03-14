94d9c4e80985ecb22528259eeedd3166bcc05f13
KrishnaShah <Krishna1234shah@gmail.com>
2024-03-14 16:16:41 +0545

    first commit

--- controllers/url.js
+++ controllers/url.js
@@ -10,7 +10,10 @@ async function handelGenerateNewShortURL(req,res){
         redirectURL:body.url,
         visitHistory:[],
     });
-    return res.json({id:shortID});
+    return res.render("home",{
+        id: shortID,
+    });
+
 }
 
 async function handelGetAnalytics(req,res){
--- index.js
+++ index.js
@@ -4,16 +4,26 @@ const { connectDB } = require("./connect"); // Import the connectDB function
 const URL = require("./models/url")
 const app = express();
 const PORT = 5001;
+const path=require('path')
+const staticrouter=require("./routes/staticrouter")
 
 connectDB(`mongodb://127.0.0.1:27017/short-url`)
   .then(() => console.log("Krishna! MongoDB Connected"))
    // Handle connection errors
 
-   app.use(express.json());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
+
+app.use("/",staticrouter);
 
 app.use("/url", urlRoute);
 
-app.get("/:shortId",async (req, res)=>{
+
+
+app.get("/url/:shortId",async (req, res)=>{
   const shortId=req.params.shortId;
   const entry =await URL.findOneAndUpdate({
     shortId
